65d56745a19e8340fecd784eec4e7cf40b48005c
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-12-29 13:03:41 +0530

    feat(ui): finalize Card component

--- apps/web/src/components/ui/card.tsx
+++ apps/web/src/components/ui/card.tsx
@@ -15,3 +15,18 @@ function CardHeader({ className, ...props }: React.HTMLAttributes<HTMLDivElement
 
 function CardTitle({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
   return <h3 className={cn('text-2xl font-semibold leading-none tracking-tight', className)} {...props} />;
+}
+
+function CardDescription({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) {
+  return <p className={cn('text-sm text-muted-foreground', className)} {...props} />;
+}
+
+function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn('p-6 pt-0', className)} {...props} />;
+}
+
+function CardFooter({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn('flex items-center p-6 pt-0', className)} {...props} />;
+}
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
